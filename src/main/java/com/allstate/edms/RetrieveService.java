a9314a6e28cb61fb0566b9e74a20c546da1b2013
JaredGordon <jgordon@pivotal.io>
2015-08-11 16:44:58 -0400

    up lib versions and handle user info.

--- src/main/java/com/allstate/edms/RetrieveService.java
+++ src/main/java/com/allstate/edms/RetrieveService.java
@@ -1,10 +1,11 @@
 package com.allstate.edms;
 
 import com.allstate.ets.tibco.schemas.edms_interfaces.retrievedocumentimpl.EDMSResponseHeader;
+import com.allstate.ets.tibco.schemas.edms_interfaces.retrievedocumentimpl.EDMSResponseHeader.UserInformation;
 import com.allstate.ets.tibco.schemas.edms_interfaces.retrievedocumentimpl.EDMSRetrieveRequestType;
 import com.allstate.ets.tibco.schemas.edms_interfaces.retrievedocumentimpl.EDMSRetrieveResponseType;
-import com.allstate.ets.tibco.schemas.edms_interfaces.retrievedocumentimpl.ObjectFactory;
 import com.allstate.ets.tibco.schemas.edms_interfaces.retrievedocumentimpl.EDMSRetrieveResponseType.EDMSRetrieveResponseDetail;
+import com.allstate.ets.tibco.schemas.edms_interfaces.retrievedocumentimpl.ObjectFactory;
 import com.example.xmlns._1367425861502.EDMSRetreiveInterface;
 
 public class RetrieveService implements EDMSRetreiveInterface {
@@ -25,14 +26,18 @@ public class RetrieveService implements EDMSRetreiveInterface {
 				.getRequestTimestamp());
 		header.setCorrelationId(edmsRetrieveRequest.getEDMSRequestHeader()
 				.getCorrelationId());
-		// header.setUserInformation(request.getEDMSRequestHeader().getUserInformation());
+
+		UserInformation ui = of.createEDMSResponseHeaderUserInformation();
+		header.setUserInformation(ui);
+		ui.setUserId(edmsRetrieveRequest.getEDMSRequestHeader()
+				.getUserInformation().getUserId());
 
 		EDMSRetrieveResponseDetail dets = of
 				.createEDMSRetrieveResponseTypeEDMSRetrieveResponseDetail();
 		response.setEDMSRetrieveResponseDetail(dets);
 		dets.setContentId(edmsRetrieveRequest.getContentId());
-		dets.setDescription("Hello world!");
 
+		dets.setDescription("Hello world!");
 		return response;
 	}
 }
